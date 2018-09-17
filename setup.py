97f71fa11e876cba84dfcd4bb3b17b0f8185b72f
Andy Jack <github@veracity.ca>
2018-09-17 16:03:35 -0400

    Test dependencies with snyk; update to test on python 3.7 release version
    - move req's to requirements.txt for snyk

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ import ast
 import io
 import re
 
-# This is necessary for Python 2.6 on Travis for some reason.
-import multiprocessing
-
 from setuptools import setup
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+requirements = [i.strip() for i in open("requirements.txt").readlines()]
 
 with io.open('minfraud/version.py', 'r', encoding='utf-8') as f:
     _version = str(ast.literal_eval(_version_re.search(f.read()).group(1)))
@@ -29,14 +28,7 @@ setup(
     include_package_data=True,
     platforms='any',
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
-    install_requires=[
-        'geoip2>=2.8.0',
-        'requests>=2.7',
-        'rfc3987',
-        'strict-rfc3339',
-        'validate_email',
-        'voluptuous',
-    ],
+    install_requires=requirements,
     extras_require={':python_version=="2.7"': ['ipaddress']},
     tests_require=['requests_mock'],
     test_suite="tests",
